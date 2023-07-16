d5d1d7298d3f08c82f00d7b36580243e1e9d0f60
MiMi <janicehsu890228@gmail.com>
2023-07-17 00:15:43 +0800

    try laravel

---
+++ resources/views/tanuki.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>tanuki</title>
+
+</head>
+
+<body>
+  二狸貓
+  <img src="/imgs/bald_seal.png" >
+</body>
+
+</html>
